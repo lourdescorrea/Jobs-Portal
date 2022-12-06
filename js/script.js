efcc4ad53516e5ec58b97be7f5c7141116d05ccb
lourdescorrea <107443940+lourdescorrea@users.noreply.github.com>
2022-12-06 19:14:12 -0300

    functions view details

--- js/script.js
+++ js/script.js
@@ -111,3 +111,50 @@ for (const btn of $$(".btn-detail")) {
     getJobs2(jobsId).then((data) => viewDetails(data));
   });
 }
+
+const viewDetails = (jobs) => {
+  $("#container").innerHTML = "";
+  $("#details").innerHTML = `
+  
+        <div class="max-w-xl flex  bg-white rounded-lg border border-gray-400 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-8">
+                  <h5 class="mb-2 text-2xl font-bold text-gray-900 dark:text-white text-center">
+                  ${jobs.name}</h5>
+  
+                  <p class="text-sm mt-2">${jobs.description}
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
+                  consectetur unde ipsum nesciunt autem deleniti ipsa rerum dolor!
+                  </p>
+                  
+                  <div class="flex justify-center items-center text-white  mt-4">
+                      <p class="inline-block bg-blue-900 rounded px-3 py-1 text-sm   mr-2 mb-2">${jobs.location}</p>
+                      <p class="inline-block bg-blue-900 rounded px-3 py-1 text-sm  mr-2 mb-2">${jobs.category}</p>
+                      <p class="inline-block bg-blue-900 rounded px-3 py-1 text-sm  mr-2 mb-2">${jobs.seniority}</p>
+                  </div>
+              
+                  <div class = "flex justify-center items-center text-white space-x-4 mt-4">
+                      <a href="#" class=" flex justify-center items-center h-8 w-20 rounded-full  bg-[#4daaf2] btn btnEdit" data-id="${jobs.id}">Editar</a>
+                      <a href="#" class=" flex justify-center items-center h-8 w-20 rounded-full bg-red-600 btn btnDelete" data-id="${jobs.id}">Eliminar</a>
+                  </div>
+             </div>
+        </div>
+      `;
+
+  for (const btn of $$(".btnEdit")) {
+    btn.addEventListener("click", () => {
+      hideElement(filters);
+      hideElement(details);
+      const jobsId = btn.getAttribute("data-id");
+      $("#submit").setAttribute("data-id", jobsId);
+      getJobs2(jobsId).then((data) => viewForms(data));
+    });
+  }
+
+  for (const btn of $$(".btnDelete")) {
+    btn.addEventListener("click", () => {
+      const jobsId = btn.getAttribute("data-id");
+      $("#delete").setAttribute("data-id", jobsId);
+      deleteJobs(jobsId);
+    });
+  }
+};
