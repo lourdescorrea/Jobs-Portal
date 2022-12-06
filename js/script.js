1f1d453b02016082d0d73d53a646f8afe4deabf7
lourdescorrea <107443940+lourdescorrea@users.noreply.github.com>
2022-12-06 19:13:10 -0300

    function generate cards

--- js/script.js
+++ js/script.js
@@ -74,3 +74,40 @@ const saveJobs = () => {
     seniority: $("#seniority").value,
   };
 };
+
+// genera la cards
+
+const renderJobs = (jobs) => {
+  $("#container").innerHTML = "";
+
+  $("#container").innerHTML += `
+        <div class="  flex flex-col p-4 w-60 bg-slate-200 drop-shadow-lg hover:scale-105 ">
+        <div class="text-gray-900 font-bold text-xl mb-2">${name}</div>
+        
+            <p class="text-sm mt-2">${description}
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
+            consectetur unde ipsum nesciunt autem deleniti ipsa rerum dolor!
+            </p>
+            
+            <div class="text-white items-center mt-4">
+                <p class="inline-block bg-blue-900 rounded px-3 py-1 text-sm   mr-2 mb-2">${location}</p>
+                <p class="inline-block bg-blue-900 rounded px-3 py-1 text-sm  mr-2 mb-2">${category}</p>
+                <p class="inline-block bg-blue-900 rounded px-3 py-1 text-sm  mr-2 mb-2">${seniority}</p>
+            </div>
+
+            <button class="px-6 m-1 w-auto h-8 text-white  bg-[#4daaf2] hover:bg-blue-600  rounded-full btn-detail" data-id="${id}">
+            Ver Detalles
+            </button>
+        </div>
+
+`;
+};
+for (const btn of $$(".btn-detail")) {
+  btn.addEventListener("click", () => {
+    hideElement(container);
+    hideElement(filters);
+    showElement(details);
+    const jobsId = btn.getAttribute("data-id");
+    getJobs2(jobsId).then((data) => viewDetails(data));
+  });
+}
